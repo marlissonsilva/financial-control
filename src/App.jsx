a210d036fdd62f867884afc430991fe5f6b55278
marlissonsilva <marlyssonsilva005@gmail.com>
2023-10-21 18:27:13 -0300

    base code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {AddTransaction} from "./components/AddTransaction";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Controle finaceiro</h1>
+      <AddTransaction />
+    </>
+  );
+}
+
+export default App;
