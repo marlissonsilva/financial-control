08e05e381d6ea6bc5cda0898529d5d4c3943b019
marlissonsilva <marlyssonsilva005@gmail.com>
2023-10-23 09:30:37 -0300

    CardTransaction Component

---
+++ src/components/CardTransaction.jsx
@@ -0,0 +1,14 @@
+export const CardTransaction = () => {
+  return (
+    <li className="transactions">
+      <p>Preço: transaction</p>
+      <p>Comprado em: transaction </p>
+      <p>Descrição: transaction</p>
+      <p>Responsável: transaction</p>
+      <div className="icons">
+        <i className="fa-solid fa-pen-to-square"></i>
+        <i className="fa-solid fa-trash"></i>
+      </div>
+    </li>
+  );
+};
