5bdb20a8a4f3966f9fe09aaf4fd992c710b829c8
marlissonsilva <marlyssonsilva005@gmail.com>
2023-11-02 22:37:44 -0300

    update firebaseConfig

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,14 @@
 import {initializeApp} from "firebase/app";
+// import {getAnalytics} from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyA2-JPwwYvpgPHps-VSbqYOaYHn46DSjzc",
+  authDomain: "controle-de-gastos-8f315.firebaseapp.com",
+  projectId: "controle-de-gastos-8f315",
+  storageBucket: "controle-de-gastos-8f315.appspot.com",
+  messagingSenderId: "950556322482",
+  appId: "1:950556322482:web:fe7a0ffadae0c0f826b7ab",
+  measurementId: "G-E63L4R0BFQ",
 };
 
 // Initialize Firebase
